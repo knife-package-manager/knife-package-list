a03aa65ee08869a89041e0f37da68eb36d85f137
17do <fucho.021000@gmail.com>
2024-08-22 22:09:27 +0900

    update list

--- create_package.bash
+++ create_package.bash
@@ -3,7 +3,11 @@ cd "$1"
 touch "dependencies"
 touch "language"
 touch "repository"
+touch "version"
+touch "capacity"
 
 echo "$2" >dependencies
 echo "$3" >language
 echo "$4" >repository
+echo "$5" >capacity
+echo "$6" >version
