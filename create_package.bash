1e85af4aacf94c2ea49ec3b19795366555bd0ece
17do <fucho.021000@gmail.com>
2024-09-22 16:54:37 +0900

    commit

--- create_package.bash
+++ create_package.bash
@@ -1,13 +1,4 @@
 mkdir "$1"
 cd "$1"
-touch "dependencies"
-touch "language"
-touch "repository"
-touch "version"
-touch "capacity"
-
-echo "$2" >dependencies
-echo "$3" >language
-echo "$4" >repository
-echo "$5" >capacity
-echo "$6" >version
+package="$1/package.toml"
+printf "dependencies = \"$2\"\nlanguage = \"$3\"\nrepository = \"$4\"\ncapacity = $5\nversion = \"$6\"\ndownload = $7" >"$1/package.toml"
